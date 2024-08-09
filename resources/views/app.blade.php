97d1e6394976e01f08c684b1e60395a96dbafc0a
Maram <75023859+maram333@users.noreply.github.com>
2024-08-09 16:18:31 +0300

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,1 +1,22 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+        {{-- START HEADER --}}
+        @include ('header')
+        {{-- END HEADER --}}
+        
+        {{-- START CONTENT --}}
+        @extends ('content')
+        {{-- END CONTENT --}}
+        
+        {{-- START FOOTER --}}
+        @include ('footer')
+        {{-- END FOOTER --}}
+        
+    </body>
+</html>
